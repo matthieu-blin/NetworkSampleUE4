5f5057acdf2eff76a0fea0237b88b875b621a07e
matthieu.blin <matthieu.blin@pieceofcake-studios.com>
2023-11-10 18:47:51 +0100

    Update a bit of cpp anf fixes

---
+++ Source/NetworkSample/MyCharacter.cpp
@@ -0,0 +1,38 @@
+﻿#include "MyCharacter.h"
+#include "Net/UnrealNetwork.h"
+#include "Logging/StructuredLog.h"
+
+// Définit les valeurs par défaut
+AMyCharacter::AMyCharacter()
+{
+	// Définissez ce character pour appeler Tick() à chaque frame. Vous pouvez désactiver cette option pour améliorer les performances si vous n’en avez pas besoin.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Appelé au début du jeu ou au moment de l'apparition de l'animal.
+void AMyCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Appelé chaque frame
+void AMyCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Appelé pour lier la fonctionnalité à l’entrée
+void AMyCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
+void AMyStaticMeshActor::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	DOREPLIFETIME(AMyStaticMeshActor, PouetCPP);
+}
+
+void AMyStaticMeshActor::MultiCastCPP_Implementation()
+{
+	UE_LOGFMT(LogNet, Error, "Un petit truc");
+}
---
+++ Source/NetworkSample/MyCharacter.h
@@ -0,0 +1,42 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Engine/StaticMeshActor.h"
+#include "MyCharacter.generated.h"
+
+UCLASS()
+class NETWORKSAMPLE_API AMyCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Définit les valeurs par défaut des propriétés de ce character
+	AMyCharacter();
+
+protected:
+	// Appelé au début du jeu ou au moment de l'apparition de l'animal.
+	virtual void BeginPlay() override;
+
+public:
+	// Appelé chaque frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Appelé pour lier la fonctionnalité à l’entrée
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+};
+
+UCLASS()
+class AMyStaticMeshActor : public AStaticMeshActor
+{
+	GENERATED_BODY()
+
+public :
+
+	UPROPERTY(Replicated, BlueprintReadWrite)
+	int PouetCPP = 0;
+
+	UFUNCTION(NetMulticast, Reliable, BlueprintCallable, Category = "Online|MyStaticMeshActor")
+	void MultiCastCPP();
+
+};
